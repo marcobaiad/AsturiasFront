68015a8cc3ad4740bc1873c5f2d313ae5ba9089d
Marco Baiad <marcobaiad@gmail.com>
2020-08-07 10:30:11 -0300

    arreglos en integración cloudinary

--- src/pages/PlatoPage.jsx
+++ src/pages/PlatoPage.jsx
@@ -39,7 +39,7 @@ const PlatosPage = () => {
             <h3 className="mt-5">{platoID.title}</h3>
             <hr className="bg-white" />
             <div className="row justify-content-center">
-                <img className="platoIMG col-12 col-md-6 px-2 px-md-0" src={p.imageUrl.includes('cloudinary') ? platoID.imageUrl : `http://asturiasrestorant.herokuapp.com` + platoID.imageUrl} alt={platoID.title} />
+                <img className="platoIMG col-12 col-md-6 px-2 px-md-0" src={platoID.imageUrl.includes('cloudinary') ? platoID.imageUrl : `http://asturiasrestorant.herokuapp.com` + platoID.imageUrl} alt={platoID.title} />
                 <div className="col-12 col-md-6 px-2">
                     <p>{platoID.description}</p>
                     <p>{platoID.summary}</p>

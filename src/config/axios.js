80a96a0620f0d9bb3a3985475d117b6b6e8c8cbd
Marco Baiad <marcobaiad@gmail.com>
2020-08-11 22:25:37 -0300

    si

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clienteAxios = axios.create({
-  baseURL: 'http://asturiasrestorant.herokuapp.com'
+  baseURL: 'https://asturiasrestorant.herokuapp.com'
   // baseURL: 'http://localhost:3001'
 });
 

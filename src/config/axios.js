aec834e146245d48cbc00630c72ab7987f2f0c03
Marco Baiad <marcobaiad@gmail.com>
2020-08-11 21:09:40 -0300

    ultimos cambios de nuevo

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clienteAxios = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://asturiasrestorant.herokuapp.com'
 });
 
 export default clienteAxios;

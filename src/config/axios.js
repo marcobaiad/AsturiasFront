e2af2ba082878caa20a236de2ba70d5463502bb0
Marco Baiad <marcobaiad@gmail.com>
2020-08-11 22:16:30 -0300

    cambio en base url

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const clienteAxios = axios.create({
-  // baseURL: 'http://asturiasrestorant.herokuapp.com'
-  baseURL: 'http://localhost:3001'
+  baseURL: 'http://asturiasrestorant.herokuapp.com'
+  // baseURL: 'http://localhost:3001'
 });
 
 export default clienteAxios;
